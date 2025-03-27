6364b98876652ca607eda86e566db5e4ce1cc68b
Joel Kram <joelnskra@gmail.com>
2025-03-27 14:39:20 +0000

    Add dynamic dot generation and update event listener for list items

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const contentMap = {
   project3: 'thLorem ipsum dolor sit amet consectetur adipisicing elit. Vel id error, assumenda quisquam dolore architecto corporis saepe cupiditate nemo nam esse ratione ipsum at, porro, cum autem! Neque, itaque eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem natus doloremque quam voluptatibus, suscipit nulla. Nisi tempore, rerum fugit suscipit fugiat, hic saepe dignissimos officiis animi quibusdam ducimus, facere accusamus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel id error, assumenda quisquam dolore architecto corporis saepe cupiditate nemo nam esse ratione ipsum at, porro, cum autem! Neque, itaque eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem natus doloremque quam voluptatibus, suscipit nulla. Nisi tempore, rerum fugit suscipit fugiat, hic saepe dignissimos officiis animi quibusdam ducimus, facere accusamus!t 3 ok'
 }
 
-// add event listener for list items
+// add event listener for list items and update content when clicked on list
 listItems.forEach((listItems) => {
   listItems.addEventListener('click', (e) => {
     const key = e.target.getAttribute('data-key');
@@ -36,3 +36,35 @@ listItems.forEach((listItems) => {
     };
   })
 })
+
+// add dots to index line
+// Select the parent container where the dots will be added
+const indexBox = document.getElementById('index-box');
+
+// Function to add a line of dots dynamically
+function addDotsLine() {
+  // Create a container for the dots
+  const lineContainer = document.createElement('div');
+
+  // Create the dots
+  for (let i = 0; i < 1; i++) {
+    const dot = document.createElement('span');
+    dot.classList.add('dot');
+    lineContainer.appendChild(dot); // Add each dot to the container
+  }
+
+  // Add a line break after the dots
+  const lineBreak = document.createElement('br');
+  lineContainer.appendChild(lineBreak);
+
+  // Append the line container to the parent container
+  indexBox.appendChild(lineContainer);
+}
+
+let listLength = Object.keys(contentMap).length; // length of content map 
+console.log(listLength);
+
+// Add multiple lines of dots dynamically
+for (let i = 0; i < listLength; i++) { // adds as many contentMap items there are
+  addDotsLine();
+}
