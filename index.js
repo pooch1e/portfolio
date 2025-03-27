f2a7f3db83bf245b8e17662036e668051316a2f5
Joel Kram <joelnskra@gmail.com>
2025-03-27 14:17:26 +0000

    Added more styles to list and dynamic content to projects

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-// start small, target about me div to fill text when page called and when intro is clicked
-
-// capture intro div text box
-const pageContent = document.getElementById('page-content');
-const rightFrameDiv = document.getElementById('right-frame');
+// capture elements
 const aboutTextDiv = document.getElementById('about-text-box');
-const introDiv = document.getElementById('li');
-const introTextBox = document.getElementById('text-box')
-// text box intro = joel at the beginning
-// introTextBox.innerHTML = 'JOEL KRAM';
+const listItems = document.querySelectorAll('li');
+const headerTextBox = document.getElementById('text-box');
 
+// object for text 
+const contentMap = {
+  intro: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel id error, assumenda quisquam dolore architecto corporis saepe cupiditate nemo nam esse ratione ipsum at, porro, cum autem! Neque, itaque eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem natus doloremque quam voluptatibus, suscipit nulla. Nisi tempore, rerum fugit suscipit fugiat, hic saepe dignissimos officiis animi quibusdam ducimus, facere accusamus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel id error, assumenda quisquam dolore architecto corporis saepe cupiditate nemo nam esse ratione ipsum at, porro, cum autem! Neque, itaque eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem natus doloremque quam voluptatibus, suscipit nulla. Nisi tempore, rerum fugit suscipit fugiat, hic saepe dignissimos officiis animi quibusdam ducimus, facere accusamus!',
+  project1: 'tLorem ipsum dolor sit amet consectetur adipisicing elit. Vel id error, assumenda quisquam dolore architecto corporis saepe cupiditate nemo nam esse ratione ipsum at, porro, cum autem! Neque, itaque eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem natus doloremque quam voluptatibus, suscipit nulla. Nisi tempore, rerum fugit suscipit fugiat, hic saepe dignissimos officiis animi quibusdam ducimus, facere accusamus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel id error, assumenda quisquam dolore architecto corporis saepe cupiditate nemo nam esse ratione ipsum at, porro, cum autem! Neque, itaque eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem natus doloremque quam voluptatibus, suscipit nulla. Nisi tempore, rerum fugit suscipit fugiat, hic saepe dignissimos officiis animi quibusdam ducimus, facere accusamus!',
+  project2: 'tLorem ipsum dolor sit amet consectetur adipisicing elit. Vel id error, assumenda quisquam dolore architecto corporis saepe cupiditate nemo nam esse ratione ipsum at, porro, cum autem! Neque, itaque eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem natus doloremque quam voluptatibus, suscipit nulla. Nisi tempore, rerum fugit suscipit fugiat, hic saepe dignissimos officiis animi quibusdam ducimus, facere accusamus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel id error, assumenda quisquam dolore architecto corporis saepe cupiditate nemo nam esse ratione ipsum at, porro, cum autem! Neque, itaque eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem natus doloremque quam voluptatibus, suscipit nulla. Nisi tempore, rerum fugit suscipit fugiat, hic saepe dignissimos officiis animi quibusdam ducimus, facere accusamus!hoo',
+  project3: 'thLorem ipsum dolor sit amet consectetur adipisicing elit. Vel id error, assumenda quisquam dolore architecto corporis saepe cupiditate nemo nam esse ratione ipsum at, porro, cum autem! Neque, itaque eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem natus doloremque quam voluptatibus, suscipit nulla. Nisi tempore, rerum fugit suscipit fugiat, hic saepe dignissimos officiis animi quibusdam ducimus, facere accusamus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel id error, assumenda quisquam dolore architecto corporis saepe cupiditate nemo nam esse ratione ipsum at, porro, cum autem! Neque, itaque eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem natus doloremque quam voluptatibus, suscipit nulla. Nisi tempore, rerum fugit suscipit fugiat, hic saepe dignissimos officiis animi quibusdam ducimus, facere accusamus!t 3 ok'
+}
 
-// function for when clicked to load the content
-const introClick = introDiv.addEventListener('click', (e) => {
-  console.log(e);
-  console.log("Attempting to add text to info div dynamically")
-  aboutTextDiv.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel id error, assumenda quisquam dolore architecto corporis saepe cupiditate nemo nam esse ratione ipsum at, porro, cum autem! Neque, itaque eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem natus doloremque quam voluptatibus, suscipit nulla. Nisi tempore, rerum fugit suscipit fugiat, hic saepe dignissimos officiis animi quibusdam ducimus, facere accusamus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel id error, assumenda quisquam dolore architecto corporis saepe cupiditate nemo nam esse ratione ipsum at, porro, cum autem! Neque, itaque eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem natus doloremque quam voluptatibus, suscipit nulla. Nisi tempore, rerum fugit suscipit fugiat, hic saepe dignissimos officiis animi quibusdam ducimus, facere accusamus!'
-  
-  // STARTING WITH JOELKRAM AND CHANGING TO INTRODCUTION
-  introTextBox.innerText = 'INTRODUCTION';
+// add event listener for list items
+listItems.forEach((listItems) => {
+  listItems.addEventListener('click', (e) => {
+    const key = e.target.getAttribute('data-key');
 
+    const content = contentMap[key];
+    
+
+    //update text div
+    if (content) {
+      aboutTextDiv.innerText = content;
+    }
+
+    // update title if intro is clicked
+    if (content === contentMap.intro) {
+      headerTextBox.innerText = 'INTRODUCTION'
+    } else if (content === contentMap.project1) {
+      headerTextBox.innerText = 'PROJECT 1'
+    } else if (content === contentMap.project2) {
+      headerTextBox.innerText = 'PROJECT 2'
+    } else if (content === contentMap.project3) {
+      headerTextBox.innerText = 'PROJECT 3'
+    };
+  })
 })
